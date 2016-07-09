e437106963ea5573b0ba7bd64f81f759aafc2e2b
jeffreywescott <jeffrey@learnersguild.org>
2016-07-09 11:02:58 -0700

    remove 'FATAL' from no such subcommand error

--- src/findSubcommandDescriptor.js
+++ src/findSubcommandDescriptor.js
@@ -1,7 +1,7 @@
 export default function findSubcommandDescriptor(commandDescriptor, subcommand) {
   const subcommandDescriptor = commandDescriptor.commands.filter(cmd => cmd.name === subcommand)[0]
   if (!subcommandDescriptor) {
-    throw new Error(`FATAL: no such subcommand '${subcommand}'`)
+    throw new Error(`No such subcommand '${subcommand}'`)
   }
   return subcommandDescriptor
 }
